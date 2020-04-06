04b1484099935c5b1506a9837f025ef5d0f57aab
ahmedbenarab <ahmedswalmi@gmail.com>
2020-04-06 09:12:26 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,48 @@
+###########################################################################
+#Created By Ahmed Benarab in 2020/03/30 Whene Corana are spreading out !!!#
+#-------------------------------------------------------------------------#
+#License : the MIT license                                                #
+###########################################################################
+
+
+
+
+
+
+
 from tkinter import Tk
 import keyboard
 import time
-def printos():
-    #time.sleep(0.25)
-    clp_data= Tk().clipboard_get()
-    list1=clp_data.split('\n')
 
-    for i in range(len(list1)-1) :
-        if len(list1[i])==0:
-            list1.pop(i)
-        else:
-            pass
-    list1.insert(1, '')
-    list1.insert(2, '2')
-    for i in list1 :        
-        keyboard.write(i)
-        keyboard.press_and_release('tab',do_press=True, do_release=True)
-    end()
-keyboard.add_hotkey('ctrl+p', lambda: printos())
+class proprint:
+    def __init__(self ,sttings,hotkey):
+        self.sttings = sttings
+        self.hotkey = hotkey
+    
+    def printos(self):
+        clp_data= Tk().clipboard_get()
+        list1=clp_data.split('\n')
+
+        for i in range(len(list1)-1) :
+            if len(list1[i])==0:
+                list1.pop(i)
+            else:
+                pass
+        list1.insert(1, '')
+        list1.insert(2, self.sttings)
+        for i in list1 :        
+            keyboard.write(i)
+            keyboard.press_and_release('tab',do_press=True, do_release=True)
+
+    def listner(self):
+        keyboard.add_hotkey(self.hotkey, lambda: self.printos())
+    
+
+MCQ = proprint('1','ctrl+p')
+MCQ.listner()
+
+MonoCQ = proprint('2','ctrl+o')
+MonoCQ.listner()
+
+YN = proprint('0','ctrl+i')
+YN.listner()
